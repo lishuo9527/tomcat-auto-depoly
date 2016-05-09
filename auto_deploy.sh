1e55ac2c900768bb4409f79fcb2f231b9adba3a2
lishuo9527 <lishuo9527@gmail.com>
2016-05-09 14:01:12 +0800

    fix bug

--- auto_deploy.sh
+++ auto_deploy.sh
@@ -10,69 +10,71 @@
 usage()
 {
     echo "Usage: ${0##*/} [projName] [deploy] | deploy the specified project "
-	echo "       ${0##*/} [start|stop|restart] | start or stop or restart tomcat" 
+    echo "       ${0##*/} [start|stop|restart] | start or stop or restart tomcat" 
     exit 1
 }
 
 deploy()
 {
+	echo "tomcat path:       ${tPath}"
+	echo "tomcat war:       ${tWar}"
+	echo "ftp path:       ${ftpPath}"
+	echo "ftp war:       ${fWar}"
+
 	echo 'step1) stop tomcat'
-	pid=`ps aux|grep java|grep $tomcatPath|awk '{printf $2}'`
-	kill -9 $pid
+	bash ${tomcatPath}/bin/shutdown.sh
 	echo 'stop tomcat finished...Done! '
 
 	echo 'step2) start tomcat'
-	rm -rf $tPath
-	rm -rf $tWar
-	cp $fWar $tWar
-	echo 'copy $projName.war file ... Done!'
+	rm -rf ${tPath}
+	rm -rf ${tWar}
+	cp ${fWar} ${tWar}
+	echo "copy ${projName}.war file ... Done!"
 
-	bash $tomcatPath/bin/startup.sh
+	bash ${tomcatPath}/bin/startup.sh
 	echo 'Done!'
 
 	echo 'show logs? y/n'
 	read command
-	if [ "$command" == "y" ]; then
-		tail -f $tomcatPath/logs/catalina.out
+	if [ "${command}" == "y" ]; then
+		tail -f ${tomcatPath}/logs/catalina.out
 	fi
 }
 
 restart()
 {
 	echo 'step1) stop tomcat'
-	pid=`ps aux|grep java|grep $tomcatPath|awk '{printf $2}'`
-	kill -9 $pid
+	bash ${tomcatPath}/bin/shutdown.sh
 	echo 'stop tomcat finished...Done! '
 
 	echo 'step2) start tomcat'
-	bash $tomcatPath/bin/startup.sh
+	bash ${tomcatPath}/bin/startup.sh
 	echo 'Done!'
 
 	echo 'show logs? y/n'
 	read command
 	if [ "$command" == "y" ]; then
-		tail -f $tomcatPath/logs/catalina.out
+		tail -f ${tomcatPath}/logs/catalina.out
 	fi
 }
 
 start()
 {
 	echo 'start tomcat...'
-	bash $tomcatPath/bin/startup.sh
+	bash ${tomcatPath}/bin/startup.sh
 	echo -n 'Done!'
 
 	echo 'show logs? y/n'
 	read command
 	if [ "$command" == "y" ]; then
-		tail -f $tomcatPath/logs/catalina.out
+		tail -f ${tomcatPath}/logs/catalina.out
 	fi
 }
 
 stop()
 {
 	echo 'stop tomcat...'
-	pid=`ps aux|grep java|grep $tomcatPath|awk '{printf $2}'`
-	kill -9 $pid
+	bash ${tomcatPath}/bin/shutdown.sh
 	echo 'Done! '
 }
 
@@ -81,12 +83,12 @@ projName=$1
 
 ## FTP 上传路径
 ftpPath=/usr/local/tomcat/temp
-fWar=$ftpPath/$projName.war
+fWar=${ftpPath}/${projName}.war
 
 ## tomcat 路径
 tomcatPath=/usr/local/tomcat
-tPath=$tomcatPath/$projName
-tWar=$tomcatPath/$projName.war
+tPath=${tomcatPath}/webapps/${projName}
+tWar=${tomcatPath}/webapps/${projName}.war
 
 
 if [ "$2" == "deploy" ]; then
